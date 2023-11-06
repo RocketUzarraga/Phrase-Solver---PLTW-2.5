ff88ddfca5af8af1e01be265bec9a6467fdcbf1b
RocketUzarraga <uzarraga9080@mydusd.org>
2023-11-05 19:13:43 -0800

    Adding replay and game setup

--- PhraseSolver.java
+++ PhraseSolver.java
@@ -1,6 +1,5 @@
 import java.util.*;
 
-
 /**
  * Phrase Solver, a class to run a game where players guess letters
  * or full phrases in order to correctly discover a secret phrase.
@@ -19,12 +18,11 @@ class PhraseSolver {
     private String guessed = "";
     private Scanner input;
 
-
     /**
      * Constructor for PhraseSolver class. Intializes a board object
-     * and an amount of Player objects. Takes a paramater for amount of 
-     * players as well as the names of the players,setting them to 
-     * "Player #" by default if no input entered. Uses a precondition 
+     * and an amount of Player objects. Takes a paramater for amount of
+     * players as well as the names of the players,setting them to
+     * "Player #" by default if no input entered. Uses a precondition
      * that np > 1 to set up a proper multiplayer game to play.
      * 
      * @param np - an int param to determine amount of players
@@ -51,45 +49,60 @@ class PhraseSolver {
         System.out.println("\nWelcome to Phrase Solver (not a Wheel of Fortune ripoff)\n");
     }
 
-
-
+    /**
+     * A play method for the PhraseSolver class, to initiate a game loop.
+     * Will go through each Player in the ArrayList, take their input, and determine
+     * game actions based on their inputs, and change scores accordingly.
+     */
     public void play() {
         String guess = "";
         // Game loop. Designed to loop through array list indexes.
-        for (int turn = 0; !solved; turn = (turn +1) % players.size()) {
+        for (int turn = 0; !solved; turn = (turn + 1) % players.size()) {
             // Change turn
             Player current = players.get(turn);
-            
+            // Print game info
             System.out.println(current.getName() + "'s turn. Current money: $" + current.getScore());
-            System.out.println("Your wheel spin: $"  + board.getLetterValue());
+            System.out.println("Your wheel spin: $" + board.getLetterValue());
             System.out.println("Guesed letters: " + guessed);
             System.out.println("Game progress: " + board.gameStaus());
             System.out.print("Make a guess: ");
 
+            // Get user input (strip spaces, uniform letter casing)
             guess = input.nextLine().strip().toUpperCase();
 
+            // Phrase guess conditional
             if (guess.length() > 1) {
+                // Guess is correct?
                 if (board.isSolved(guess)) {
                     solved = true;
                     current.setScore(current.getScore() + board.getLetterValue());
-                    System.out.println("Congrats " + current.getName() +"! You get $" + current.getScore() + "!");
+                    System.out.println("Congrats " + current.getName() + "! You get $" + current.getScore() + "!");
+                    // Guess is not correct
                 } else {
                     System.out.println("Your guess was incorrect. Skill issue.");
                 }
+
+                // Letter guess conditional
             } else {
+                // Prevent guessing of same letter
                 if (guessed.contains(guess)) {
                     System.out.println("That's been guessed already. Bad. Skipping your turn.");
+
                 } else {
                     guessed += guess + " ";
+                    // Guess is in phrase?
                     if (board.guessLetter(guess)) {
                         System.out.println("That's in the phrase! Adding $" + board.getLetterValue());
                         current.setScore(current.getScore() + board.getLetterValue());
+                        // Guess isn't in phrase.
                     } else {
                         System.out.println("That's not it! You lose your turn.");
                     }
                 }
             }
+            // Line spacing
             System.out.println("\n");
+            // Set new letter value
             board.setLetterValue();
         }
     }
--- Runner.java
+++ Runner.java
@@ -1,10 +1,39 @@
 import java.util.Scanner;
+
 public class Runner {
     public static void main(String[] args) {
-        // Code in replay loop and input for amt of players later.
+        boolean replay = true;
+        PhraseSolver ps;
+        int numPlayers;
+
         Scanner input = new Scanner(System.in);
-        PhraseSolver ps = new PhraseSolver(2, input);
-        ps.play();
+        while (replay) {
+            // Input number of players
+            System.out.print("How many players (2-4)? ");
+            try {
+                numPlayers = Integer.parseInt(input.nextLine());
+                // Limit between 2-4 players
+                if (numPlayers < 2 || numPlayers > 4) {
+                    System.out.println("Between 2-4 players!");
+                    continue;
+                }
+
+                // Exception if input isn't number
+            } catch (Exception e) {
+                System.out.println("Invalid. Try again.");
+                continue;
+            }
+            // Initialize and start Phrase Solver game
+            ps = new PhraseSolver(numPlayers, input);
+            ps.play();
+
+            // Replay loop
+            System.out.print("\nPlay again (y/n)? ");
+            if (!input.nextLine().toUpperCase().equals("Y")) {
+                replay = false;
+            }
+        }
+        System.out.println("Thank you for playing!");
         input.close();
     }
 }
